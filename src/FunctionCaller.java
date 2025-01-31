1e9fbdebfcb6df6d14e110e4187873bbd544d771
reb00250 <167830101+reb00250@users.noreply.github.com>
2025-01-31 14:27:32 +0900

    Create FunctionCaller.java

---
+++ src/FunctionCaller.java
@@ -0,0 +1,12 @@
+echo 'import CodeSearchProject2.Util;
+import CodeSearchProject3.Util;
+
+public class FunctionCaller {
+    public static void main(String[] args) {
+        CodeSearchProject2.Util util1 = new CodeSearchProject2.Util();
+        util1.performAction();
+
+        CodeSearchProject3.Util util2 = new CodeSearchProject3.Util();
+        util2.performAction();
+    }
+}
