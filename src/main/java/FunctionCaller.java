1143c1ebf176e4227b514f6f0bde51db50a9be7b
reb00250 <167830101+reb00250@users.noreply.github.com>
2025-02-07 14:36:32 +0900

    Update FunctionCaller.java

--- src/main/java/FunctionCaller.java
+++ src/main/java/FunctionCaller.java
@@ -1,8 +1,5 @@
 package com.example.project1;
 
-import com.example.project2.Util;
-import com.example.project3.Util;
-
 public class FunctionCaller {
     public static void main(String[] args) {
         com.example.project2.Util util1 = new com.example.project2.Util();
